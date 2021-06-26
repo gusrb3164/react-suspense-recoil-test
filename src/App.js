6360071484438b9ef9555a43fecfdbd90eb5bda8
hyun gyu <gusrb3164@gmail.com>
2021-06-26 14:27:52 +0900

    feat: first init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="App"></div>
+    </RecoilRoot>
   );
 }
 
